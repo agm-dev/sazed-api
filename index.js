a3f12823bccdcca5a3f344845b7b01ac4e079f33
Adrián Gonzalo <gonzahh@gmail.com>
2019-10-14 07:04:08 +0200

    removed old stuff

--- index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./src/config/server');
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`server running on port ${port}`);
-});
